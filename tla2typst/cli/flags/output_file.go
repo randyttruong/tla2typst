e8a820bcf120502410526baef3300afa45c3e5ff
Randy Truong <rtruong@u.northwestern.edu>
2024-12-21 01:07:17 -0600

    feat: added flags for output file options

--- tla2typst/cli/flags/output_file.go
+++ tla2typst/cli/flags/output_file.go
@@ -1,10 +1,90 @@
 package flags
 
-var (
-	outputFileNameDefault = ""
-	outputFileName        string
+import (
+	"github.com/spf13/cobra"
 )
 
 var (
-	outputFileNameFlagName = "out"
+	outputFilenameDefault = "" // TODO: Make this the .tla input filename
+	outputFilename        string
+
+	alignmentOutputFilenameDefault = "" // TODO: Make this the .tla input filename
+	alignmentOutputFilename        string
+
+	tlaOutputFilenameDefault = "" // TODO: Make this the .tla input filename
+	tlaOutputFilename        string
+
+	stylePackageFilenameDefault = "" // NOTE: Make this none by default
+	stylePackageFilename        string
 )
+
+var (
+	outputFilenameFlagName          = "out"
+	alignmentOutputFilenameFlagName = "alignOut"
+	tlaOutputFilenameFlagName       = "tlaOut"
+	stylePackageFilenameFlagName    = "style"
+)
+
+func AddOutputFileFlags(c *cobra.Command) {
+	addOutputFilenameFlag(c)
+	addAlignmentOutputFilenameFlag(c)
+	addTlaOutputFilenameFlag(c)
+	addStylePackageFilenameFlag(c)
+}
+
+func OutputFilename() string {
+	return outputFilename
+}
+
+func AlignmentOutputFilename() string {
+	return alignmentOutputFilename
+}
+
+func TlaOutputFilename() string {
+	return tlaOutputFilename
+}
+
+func StylePackageFilename() string {
+	return stylePackageFilename
+}
+
+// TODO: Add descriptors
+func addOutputFilenameFlag(c *cobra.Command) {
+	c.PersistentFlags().StringVarP(
+		&outputFilename,
+		outputFilenameFlagName,
+		"",
+		outputFilenameDefault,
+		"",
+	)
+}
+
+func addAlignmentOutputFilenameFlag(c *cobra.Command) {
+	c.PersistentFlags().StringVarP(
+		&alignmentOutputFilename,
+		alignmentOutputFilenameFlagName,
+		"",
+		alignmentOutputFilenameDefault,
+		"",
+	)
+}
+
+func addTlaOutputFilenameFlag(c *cobra.Command) {
+	c.PersistentFlags().StringVarP(
+		&tlaOutputFilename,
+		tlaOutputFilenameFlagName,
+		"",
+		tlaOutputFilenameDefault,
+		"",
+	)
+}
+
+func addStylePackageFilenameFlag(c *cobra.Command) {
+	c.PersistentFlags().StringVarP(
+		&stylePackageFilename,
+		stylePackageFilenameFlagName,
+		"",
+		stylePackageFilenameDefault,
+		"",
+	)
+}
