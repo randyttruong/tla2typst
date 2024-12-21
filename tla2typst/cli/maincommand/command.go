4c7914f2fe873f7e5f431c347603cae52e35e53d
Randy Truong <rtruong@u.northwestern.edu>
2024-12-21 00:03:38 -0600

    feat: added global filename variable and root cmd run behavior

--- tla2typst/cli/maincommand/command.go
+++ tla2typst/cli/maincommand/command.go
@@ -8,15 +8,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	Filename = ""
+)
+
 func Command() *cobra.Command {
 	c := &cobra.Command{
 		SilenceUsage: true,
 		Use:          os.Args[0],
+		Short:        "Read and process a TLA specification.",
+		Args:         cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			Filename = args[0]
+		},
 	}
 
 	flags.AddOutputFormatFlags(c)
 
-	// c.AddCommand()
+	c.AddCommand()
 
 	return c
 }
