d1b7ebd5c80fde18d0d1afadc1098f5a914435a6
Randy Truong <rtruong@u.northwestern.edu>
2024-12-22 00:30:27 -0600

    feat: finished proto Token struct + getters/setters

--- tla2typst/scanner/token.go
+++ tla2typst/scanner/token.go
@@ -1,31 +1,30 @@
 package scanner
 
-type TokenType int 
+type TokenType int
 
-const ( 
-	KEYWORD TokenType = iota 
-	IDENTIFIER 
-	LITERAL 
-	OPERATOR 
+const (
+	KEYWORD TokenType = iota
+	IDENTIFIER
+	LITERAL
+	OPERATOR
 	SPECIAL
 	NONE
 )
- 
-type Token struct { 
-	tokenType TokenType 
-	value string 
-	pos int 
-}  
 
-func (t *Token) TokenType() TokenType { 
+type Token struct {
+	tokenType TokenType
+	value     string
+	pos       int
+}
+
+func (t *Token) TokenType() TokenType {
 	return t.tokenType
-} 
+}
 
-func (t *Token) value() string { 
+func (t *Token) GetValue() string {
 	return t.value
 }
 
-func (t *Token) pos() string ( 
+func (t *Token) GetPos() int {
 	return t.pos
-)
-
+}
