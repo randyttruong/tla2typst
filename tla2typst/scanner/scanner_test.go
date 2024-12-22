ae885c11c110e2154cce6642e05553a23368bff5
Randy Truong <rtruong@u.northwestern.edu>
2024-12-22 07:05:15 -0600

    test: general scanning tests + micro scanning tests

--- tla2typst/scanner/scanner_test.go
+++ tla2typst/scanner/scanner_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestScanContent(t *testing.T) {
+func TestScanContentWithFiles(t *testing.T) {
 
 	// TODO: Use a smaller one, this is just for testing the output in terminal.
 	tests := []struct {
@@ -21,6 +21,50 @@ func TestScanContent(t *testing.T) {
 		{
 			testName: "Caesar Consensus (Short)",
 			filename: smallTestFile2,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+
+			scanner := GetScanner()
+
+			// 1. Ingest document
+			err := LoadDocument(tc.filename)
+			assert.NoError(t, err, "There should be no issues with loading the test algorithms.\n")
+
+			// 2. Initialize the scanner
+			err = InitScanner(GetLoader())
+			assert.NoError(t, err, "There should be no issues with populating the loader buf.\n")
+
+			// 3. Scan Content
+			err = scanner.ScanContent()
+			assert.NoError(t, err, "There should be no issues with scanning.\n")
+
+			// TODO: Check the tokens
+		})
+	}
+}
+
+func TestScanContentWithSpecificCases(t *testing.T) {
+
+	// TODO: Use a smaller one, this is just for testing the output in terminal.
+	tests := []struct {
+		testName string
+		filename string
+		err      error
+	}{
+		{
+			testName: "Set Comprehension",
+			filename: setComprehension,
+		},
+		{
+			testName: "Set Definition and Indexing",
+			filename: setIndexing,
+		},
+		{
+			testName: "Functional Operators",
+			filename: functionalOperators,
 		},
 	}
 
