d700457202287590450f4a62c01ac53f9b0b823e
Randy Truong <rtruong@u.northwestern.edu>
2024-12-22 00:32:40 -0600

    feat: changed run to runE and added scanner + loader to execution cycle

--- tla2typst/cli/maincommand/command.go
+++ tla2typst/cli/maincommand/command.go
@@ -1,8 +1,10 @@
 package maincommand
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/pkg/errors"
 	"github.com/randyttruong/tla2typst/cli/flags"
 	"github.com/randyttruong/tla2typst/scanner"
 	"github.com/spf13/cobra"
@@ -18,10 +20,26 @@ func Command() *cobra.Command {
 		Use:          os.Args[0],
 		Short:        "Read and process a TLA specification.",
 		Args:         cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			Filename = args[0]
 
-			scanner.LoadDocument(Filename) // TODO: Does this belong here?
+			err := scanner.LoadDocument(Filename) // TODO: Does this belong here?
+
+			if err != nil {
+				return errors.Wrapf(err, "Unable to load document %v, got %v\n", Filename, err)
+			}
+
+			fmt.Printf("Beginning parsing\n")
+
+			loader := scanner.GetLoader()
+
+			err = scanner.InitScanner(loader)
+
+			if err != nil {
+				return errors.Wrapf(err, "Unable to start scanner, got %v", err)
+			}
+
+			return nil
 		},
 	}
 
