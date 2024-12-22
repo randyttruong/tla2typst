01b13cbe8ce604020ebca1376402da633b5c5b92
Randy Truong <rtruong@u.northwestern.edu>
2024-12-21 19:25:30 -0600

    chore: moved loading functions to loader.go

--- tla2typst/scanner/scanner.go
+++ tla2typst/scanner/scanner.go
@@ -1,70 +1,7 @@
 package scanner
 
-import (
-	"fmt"
-	"os"
-	"os/user"
-	"syscall"
-
-	"github.com/pkg/errors"
-)
-
-type Scanner struct {
-	fileBytes []byte
+type ScannerState struct {
 }
 
-func checkFilePermissionsAndOwnership(filepath string) error {
-	fileInfo, err := os.Stat(filepath)
-
-	if err != nil {
-		return errors.Wrapf(err, "Failed to stat file: %v", err)
-	}
-
-	fileMode := fileInfo.Mode().Perm()
-
-	if fileMode != 0o600 {
-		return errors.Wrapf(err, "File does not have 600 permissions, got: %v", fileMode)
-	}
-
-	fileOwner, ok := fileInfo.Sys().(*syscall.Stat_t)
-
-	if !ok {
-		return errors.Wrapf(err, "Failed to get system info")
-	}
-
-	fileOwnerUid := fmt.Sprintf("%d", fileOwner.Uid)
-
-	currentUser, err := user.Current()
-
-	if err != nil {
-		return errors.Wrapf(err, "Failed to get current user: %v", err)
-	}
-
-	if fileOwnerUid != currentUser.Uid {
-		return errors.Wrapf(err, "File is not owned by current user, instead: %v", fileOwnerUid)
-	}
-
-	return nil
-}
-
-func LoadDocument(filepath string) error {
-	err := checkFilePermissionsAndOwnership(filepath)
-
-	if err != nil {
-		errors.Wrapf(err, "Failed to load document, instead got: %v", err)
-	}
-
-	bytes, err := os.ReadFile(filepath)
-
-	if err != nil {
-		if os.IsNotExist(err) {
-			return errors.Wrapf(err, "File at %v does not exist. Existing", filepath)
-		}
-
-		return errors.Wrapf(err, "Failed to read file, got %v", err)
-	}
-
-	// TODO: Write the actual parser
-
-	return nil
+type ScannerStateDelta struct {
 }
