9408990a654181a5511eae4ee824787ffbc6f958
Randy Truong <rtruong@u.northwestern.edu>
2024-12-22 00:28:37 -0600

    feat: created scanner struct and init function

--- tla2typst/scanner/scanner.go
+++ tla2typst/scanner/scanner.go
@@ -1,6 +1,35 @@
 package scanner
 
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// ScannerState represents the scanner's current state.
 type ScannerState struct {
+	loader *Loader
+	Tok    []Token
+	val    int
+}
+
+var (
+	Scanner *ScannerState
+)
+
+// InitScanner passes the loader into ScannerState
+func InitScanner(loader *Loader) error {
+	if loader == nil || GetLoader().buf == nil {
+		return errors.New("Loader or buffer does not exist/is empty.")
+	}
+
+	Scanner.loader = loader
+
+	return nil
+}
+
+func GetScanner() *ScannerState {
+	return Scanner
 }
 
 type ScannerStateDelta struct {
