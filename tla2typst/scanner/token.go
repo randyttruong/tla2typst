1ea8376d3f58727f05fa32f2a985813c072d2709
Randy Truong <rtruong@u.northwestern.edu>
2024-12-22 07:02:16 -0600

    feat: added operator, func operator, and delimiter collections

--- tla2typst/scanner/token.go
+++ tla2typst/scanner/token.go
@@ -5,10 +5,89 @@ type TokenType int
 const (
 	KEYWORD TokenType = iota
 	IDENTIFIER
-	LITERAL
+	STRING_LITERAL
+	NUM_LITERAL
 	OPERATOR
 	SPECIAL
+	DELIMITER
 	NONE
+)
+
+var (
+	KEYWORDS map[string]bool = map[string]bool{
+		"MODULE":    true,
+		"EXTENDS":   true,
+		"CONSTANT":  true,
+		"VARIABLES": true,
+	}
+
+	OPERATORS map[string]bool = map[string]bool{
+		// + LOGICAL OPERATORS
+		"/\\": true,
+		"\\/": true,
+		"~":   true,
+		"=>":  true,
+		"<=>": true,
+		// + QUANTIFIERS
+		"\\E": true,
+		"\\A": true,
+		// + SET OPERATORS
+		"\\in":       true,
+		"\\notin":    true,
+		"\\subseteq": true,
+		"\\supseteq": true,
+		"\\subset":   true,
+		"\\supset":   true,
+		"\\union":    true,
+		"\\cap":      true,
+		"\\setminus": true,
+		// + ARITHMETIC OPERATORS
+		"+":     true,
+		"-":     true,
+		"*":     true,
+		"\\div": true,
+		"^":     true,
+		// + RELATIONAL OPERATORS
+		"=":  true,
+		"#":  true,
+		">":  true,
+		"<":  true,
+		"<=": true,
+		">=": true,
+		// + MISC OPERATORS
+		"==":        true,
+		"<<":        true,
+		">>":        true,
+		"..":        true,
+		"\\":        true,
+		"|–>":       true,
+		"UNCHANGED": true,
+		// + TEMPORAL LOGIC OPERATORS
+		"[]": true,
+		"<>": true,
+		"~>": true,
+		// + NOTE THAT FUNCTIONAL OPERATORS, LIKE
+		// SEQUENCES, MUST BE CHECKED MANUALLY, AND
+		// ARE HANDLED SEPARATELY
+	}
+
+	FUNCTIONAL_OPERATORS = []string{
+		"Seq",
+		"Head",
+		"Tail",
+		"Append",
+		"Len",
+	}
+
+	DELIMITERS = []string{
+		"(",
+		")",
+		"[",
+		"]",
+		"{",
+		"}",
+		",",
+	}
 )
 
 type Token struct {
