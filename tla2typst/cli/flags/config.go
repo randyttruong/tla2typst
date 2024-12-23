10173c2353c2d13ce04fc2cf77ebb49bd027b835
Randy Truong <rtruong@u.northwestern.edu>
2024-12-23 13:45:32 -0600

    feat: config reader

--- tla2typst/cli/flags/config.go
+++ tla2typst/cli/flags/config.go
@@ -63,8 +63,91 @@ func addConfig(c *cobra.Command) {
 	)
 }
 
-// TODO:
-func readConfig() {}
+func loadFlags() {
 
-// TODO:
-func LoadConfig() {}
+	if pointSizeSet {
+		config.TlaConfigs.PointSize = PointSize()
+	}
+	if textWidthSet {
+		config.TlaConfigs.TextWidth = TextWidth()
+	}
+	if textHeightSet {
+		config.TlaConfigs.TextHeight = TextHeight()
+	}
+	if horizontalOffsetSet {
+		config.TlaConfigs.HorizontalOffset = HorizontalOffset()
+	}
+	if verticalOffsetSet {
+		config.TlaConfigs.VerticalOffset = VerticalOffset()
+	}
+
+	if useShadingSet {
+		config.TlaConfigs.UseShading = UseShading()
+	}
+	if usePcalShadingSet {
+		config.TlaConfigs.UsePcalShading = UsePcalShading()
+	}
+	if commentGrayLevelSet {
+		config.TlaConfigs.CommentGrayLevel = CommentGrayLevel()
+	}
+	if createPostScriptSet {
+		config.TlaConfigs.CreatePostScript = CreatePostScript()
+	}
+
+	if outputFilenameSet {
+		config.TlaConfigs.OutputFilename = OutputFilename()
+	}
+	if alignmentOutputFilenameSet {
+		config.TlaConfigs.AlignmentOutputFilename = AlignmentOutputFilename()
+	}
+	if tlaOutputFilenameSet {
+		config.TlaConfigs.TlaOutputFilename = TlaOutputFilename()
+	}
+	if stylePackageFilenameSet {
+		config.TlaConfigs.StylePackageFilename = StylePackageFilename()
+	}
+}
+
+func readConfig(filepath string) error {
+
+	err := util.CheckFilePermissionsAndOwnership(filepath)
+
+	if err != nil {
+		errors.Wrapf(err, "Failed to load config, instead got: %v", err)
+	}
+
+	bytes, err := os.ReadFile(filepath)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return errors.Wrapf(err, "File at %v does not exist.", filepath)
+		}
+
+		return errors.Wrapf(err, "Failed to read file, got %v", err)
+	}
+
+	if err := yaml.Unmarshal(bytes, &config); err != nil {
+		return errors.Wrapf(err, "failed to retrieve config from file %q", filepath)
+	}
+
+	return nil
+}
+
+func LoadConfig(cmd *cobra.Command, args []string) error {
+
+	if configFilename == "" || !configFilenameSet {
+		return nil
+	}
+
+	err := readConfig(configFilename)
+
+	if err != nil {
+		err = errors.Errorf("Error reading instance config file: %v", err)
+		return err
+	}
+
+	// populate config with flags to overwrite config options
+	loadFlags()
+
+	return nil
+}
