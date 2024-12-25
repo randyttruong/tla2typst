9f1cea226280a1b8d39148a5b38766c29c390ff8
Randy Truong <rtruong@u.northwestern.edu>
2024-12-25 14:35:40 -0600

    feat: added parser base class; initial parser methods

--- tla2typst/scanner/parser.go
+++ tla2typst/scanner/parser.go
@@ -1,1 +1,41 @@
 package scanner
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+type ParserState struct {
+	scanner *ScannerState
+}
+
+var (
+	Parser = &ParserState{}
+)
+
+func InitParser(scanner *ScannerState) error {
+	if scanner == nil {
+		return errors.New("Scanner or stream does not exist")
+	}
+
+	Parser.scanner = scanner
+
+	return nil
+}
+
+func (p *ParserState) ParseContent() error {
+
+	if p.scanner == nil {
+		err := errors.New("Scanner or stream does not exist")
+		return err
+	}
+
+	stream := p.scanner.stream
+
+	for _, tok := range stream {
+		fmt.Printf("%v", tok)
+	}
+
+	return nil
+}
