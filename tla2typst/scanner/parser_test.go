43205cc2c078e89709ee6bc55ba4da546b13066c
Randy Truong <rtruong@u.northwestern.edu>
2024-12-25 14:36:03 -0600

    test: parser initialization definition

--- tla2typst/scanner/parser_test.go
+++ tla2typst/scanner/parser_test.go
@@ -1,1 +1,19 @@
 package scanner
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_parser_init(t *testing.T) {
+	tests := []struct {
+		testName string
+		err      error
+	}{}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			fmt.Println("Hello world!")
+		})
+	}
+}
