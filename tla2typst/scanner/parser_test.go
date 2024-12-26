eadbe3a2c687d68408893187919943f0b13b4463
Randy Truong <rtruong@u.northwestern.edu>
2024-12-25 18:21:40 -0600

    test: half test for pretty printing

--- tla2typst/scanner/parser_test.go
+++ tla2typst/scanner/parser_test.go
@@ -3,6 +3,8 @@ package scanner
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_parser_init(t *testing.T) {
@@ -14,6 +16,42 @@ func Test_parser_init(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.testName, func(t *testing.T) {
 			fmt.Println("Hello world!")
+		})
+	}
+}
+
+func Test_PrettyPrinting(t *testing.T) {
+	tests := []struct {
+		testName string
+		ast      Expr
+		expected string
+		err      error
+	}{
+		{
+			testName: "Init == 1",
+			ast: &Binary{
+				op: "==",
+				left: &Identifier{
+					value: "Init",
+				},
+				right: &Literal{
+					value: "1",
+				},
+			},
+			expected: "(== Init 1)",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+
+			var (
+				p = &PrettyPrinter{}
+			)
+
+			res := fmt.Sprintf("%v", tc.ast.accept(p))
+
+			assert.Equal(t, tc.expected, res, "Expected these to be the same")
 		})
 	}
 }
